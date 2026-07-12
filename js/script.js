9c3e29302806ce5c5b690015d4f0b3a1e5134b5b
h2mnteam <h.hekmat@ymail.com>
2026-07-12 09:28:42 +0800

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// ===========================
+// HAMBURGER MENU
+// ===========================
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+menuToggle.addEventListener("click", () => {
+
+    navMenu.classList.toggle("active");
+
+});
